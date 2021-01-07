5985669ce159740964c821be55af32bc816f316d
taemkim <temtamk@kakao.com>
2021-01-08 03:10:51 +0900

    [ADD]list add, update

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,28 @@
+import React from 'react'
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const [list, setList] = React.useState([]);
+  const [text, setText] = React.useState('');
+  React.useEffect(()=>{
+    
+  }, [])
+
+  const addItem = React.useCallback( () => {
+    const item = {
+      id: ( new Date().getTime() ).toString(),
+      text,
+    };
+    setList([
+      ...list,
+      item,
+    ]);
+  }, [list, text] );
+
+  const removeItem = React.useCallback( id => {
+    
+  })
+  
   return (
     <div className="py-8 px-16">
       <Head>
@@ -9,15 +30,29 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1 className="text-4xl">To-Do List</h1>
+      <h1 className="text-4xl font-bold">To-Do List</h1>
 
       <div>
-        <input type="text" className="border-black"/>
-        <button>추가</button>
+        <input 
+          type="text" className="border py-1" 
+          value={ text } 
+          onChange={ event => setText( event.target.value ) }/>
+        <button 
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 rounded"
+          onClick={ addItem }>
+          추가
+        </button>
       </div>
 
-      <ul>
-        <li>할 일 내용</li>
+      <ul className="list-disc">
+        {
+          list.map( item => (
+            <li key={ item.id }>
+              { item.text }
+              <button className="ml-2 text-xs text-red-500">[삭제]</button>
+            </li>
+          ))
+        }
       </ul>
     </div>
   )
